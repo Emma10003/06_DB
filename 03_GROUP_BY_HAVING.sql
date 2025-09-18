c889394284261d548f43b9fbdb7ab4b9066ae386
Emma10003 <5emma10003@gmail.com>
2025-09-18 12:59:30 +0900

    GROUP BY HAVING

--- 03_GROUP_BY_HAVING.sql
+++ 03_GROUP_BY_HAVING.sql
@@ -31,7 +31,10 @@ GROUP BY dept_id;
 -- 부서별 급여의 평균(정수 - 내림처리) AS 급여 평균
 -- 인원 수 조회 AS 인원 수
 -- 부서 ID 순으로 오름차순 정렬
-SELECT dept_id, SUM(salary) AS `급여 합계`, FLOOR(AVG(salary)) AS `급여 평균`, COUNT(emp_id) AS `인원 수`
+SELECT dept_id AS `부서아이디`,
+		SUM(salary) AS `급여 합계`, 
+        FLOOR(AVG(salary)) AS `급여 평균`, 
+        COUNT(emp_id) AS `인원 수`
 FROM employees
 GROUP BY dept_id
 ORDER BY dept_id;
@@ -108,3 +111,93 @@ SELECT dept_id, AVG(salary) AS `평균 급여`
 FROM employees
 GROUP BY dept_id
 ORDER BY dept_id;
+
+
+/************************
+WHERE 절  : 지정된 테이블에서 어떤 행만을 조회 결과로 삼을건지 조건을 지정하는 구문
+            (테이블 내에서 특정 행만 뽑아서 쓰겠다는 조건문)
+           
+HAVING 절 : 그룹함수로 구해 올 그룹에 대한 조건을 설정할 때 사용
+            (그룹에 대한 조건, 어떤 그룹만 조회하겠다)
+            
+HAVING 컬럼명 | 함수식 비교연산자 비교값
+************************/
+
+USE employee_management;
+
+SELECT * FROM employees;
+
+-- 직원이 2명 이상인 부서 보기
+SELECT dept_id, COUNT(*)
+FROM employees
+WHERE COUNT(*) >= 2; -- Error Code: 1111. Invalid use of group function
+-- 그룹 함수를 잘못 사용했을 때 나타나는 문제
+
+-- dept_id 로 묶은 그룹에서 총 인원이 2명 이상인 부서 아이디만 조회
+SELECT dept_id, COUNT(*)
+FROM employees
+GROUP BY dept_id
+HAVING COUNT(*) >= 2;
+
+/*
+WHERE : 개별 직원 조건
+급여가 5천만원 이상인 "직원" 찾기
+WHERE salary >= 50000000
+
+HAVING : 부서나 그룹 조건
+평균 급여가 5천만원 이상인 "부서" 찾기
+HAVING AVG(salary) >= 50000000
+
+GROUP BY HAVING = 함수(COUNT, AVG, SUM, MIN, MAX 등) 특정 그룹의 숫자 데이터를 활용해서 조건별로 조회할 때 사용
+*/
+
+-- 평균 급여가 7천만원 이상인 부서 조회
+-- dept_id, salary (employees)
+SELECT dept_id, FLOOR(AVG(salary))
+FROM employees
+GROUP BY dept_id
+HAVING AVG(salary) >= 70000000;
+
+-- 급여 총합이 1억 5천만원 이상인 부서 조회
+SELECT dept_id, SUM(salary)
+FROM employees
+GROUP BY dept_id
+HAVING SUM(salary) >= 150000000;
+
+-- WHERE dept_id
+-- employees E, departments D 연결
+-- 평균 급여가 8천만원 이상인 부서의 이름 조회
+SELECT D.dept_name, AVG(E.salary)
+FROM employees E, departments D
+WHERE E.dept_id = D.dept_id
+GROUP BY D.dept_name
+HAVING AVG(E.salary) >= 80000000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
